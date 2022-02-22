aa01040d2bb7b0dffedba96709f33a195e99c12b
Vatche <70888542+vatche-t@users.noreply.github.com>
2022-02-23 00:20:47 +0330

    Update dataset.R

--- dataset.R
+++ dataset.R
@@ -1,8 +1,8 @@
 
 dataset = read.csv('Data.csv')
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary),
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary)
+dataset$column1 = ifelse(is.na(dataset$column1),
+                     ave(dataset$column1, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$column1)
+dataset$Column2 = ifelse(is.na(dataset$Column2),
+                     ave(datasetColumn2, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Column2)
